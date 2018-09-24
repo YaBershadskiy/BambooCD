e4e6d4d91b6f5bb2165ea4af5e55b7fdfe9307cd
Unknown <y.bershadskiy@student.csn.khai.edu>
2018-09-24 18:53:05 +0300

    test1

--- BambooCD/Startup.cs
+++ BambooCD/Startup.cs
@@ -27,7 +27,7 @@ namespace BambooCD
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello my World!");
             });
         }
     }
